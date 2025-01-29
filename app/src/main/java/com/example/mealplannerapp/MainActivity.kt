b023935ebc99ff8149ffa8b7af57f9e08faf8120
Anthpa <anthonyparra003@gmail.com>
2025-01-29 12:01:41 -0500

    Implemented binding for views

--- app/src/main/java/com/example/mealplannerapp/MainActivity.kt
+++ app/src/main/java/com/example/mealplannerapp/MainActivity.kt
@@ -3,18 +3,34 @@ package com.example.mealplannerapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
+import com.example.mealplannerapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize UI
+        initUI()
     }
 
-    //Handles the user's selection of sign up request
-    fun signUpText (view:View) {
-        val changePage = Intent(this@MainActivity, SignUpSheet::class.java)
-        startActivity(changePage);
+    private fun initUI() {
+        binding.buttonSignin.setOnClickListener {
+            // Accessing user inputs
+            val getUserEmail = binding.editTextEmailSignin
+            val getUserPassword = binding.editTextPasswordSignin
+
+        }
+        binding.textViewSignupSheet.setOnClickListener{
+            val changePage = Intent(this@MainActivity, SignUpSheet::class.java)
+            startActivity(changePage);
+        }
     }
 }
--- app/src/main/java/com/example/mealplannerapp/SignUpSheet.kt
+++ app/src/main/java/com/example/mealplannerapp/SignUpSheet.kt
@@ -3,18 +3,33 @@ package com.example.mealplannerapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import com.example.mealplannerapp.databinding.ActivitySignupBinding
 
 class SignUpSheet : AppCompatActivity(){
+
+    private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        // Initialize View Binding
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize UI
+        initUI()
     }
 
-    //Handles the user's selection of sign up request
-    fun signInText (view: View) {
-        // Handler code here.
-        val changePage = Intent(this@SignUpSheet, MainActivity::class.java)
-        startActivity(changePage);
+    private fun initUI() {
+        binding.buttonSignup.setOnClickListener {
+            // Accessing user inputs
+            val getUserEmail = binding.editTextEmailSignup
+            val getUserPassword = binding.editTextPasswordSignup
+
+        }
+        binding.textViewSignupSheet.setOnClickListener{
+            val changePage = Intent(this@SignUpSheet, MainActivity::class.java)
+            startActivity(changePage);
+        }
     }
 }
